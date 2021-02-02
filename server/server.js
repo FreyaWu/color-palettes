57ffeab31f5dd271a00f253695806186b12221ef
freyaaaaa <freyawu10@gmail.com>
2021-02-02 14:52:17 -0700

    temp

--- server/server.js
+++ server/server.js
@@ -2,6 +2,7 @@ const express = require('express');
 const app = express();
 const path = require('path');
 const mongoose = require('mongoose');
+const methodOverride = require('method-override');
 const Artwork = require('./models/artwork');
 
 mongoose.connect('mongodb://localhost:27017/color-palette', { useNewUrlParser: true, useCreateIndex: true, useUnifiedTopology: true })
@@ -14,13 +15,50 @@ mongoose.connect('mongodb://localhost:27017/color-palette', { useNewUrlParser: t
     });
 
 app.set('views', path.join(__dirname, 'views'));
+app.use(express.urlencoded({extended: true}));
+app.use(methodOverride('_method'));
 
 app.get('/', (req, res) => {
     res.send('Welcome to homepage!');
 })
 
-app.get('/colors', (req, res) => {
-    res.send('Color page');
+app.get('/artworks', async (req, res) => {
+    const artworks = await Artwork.find({});
+    console.log(artworks); 
+    res.send('Artworks page');
+})
+
+app.get('/artworks/new', (req, res) => {// this route is to render a 'NEW' form
+
+})
+
+app.get('/artworks/:id', async (req, res) => {
+    const {id} = req.params;
+    const artwork = await Artwork.findById(id);
+    res.header('Content-Type', 'application/json');
+    res.send(JSON.stringify(artwork));
+})
+
+app.post('/artworks', async (req, res) => {// later will take user's data from the 'NEW' form
+    res.send(req.body);
+    const artwork = new Artwork(req.body.artwork);
+    await artwork.save();
+})
+
+app.get('/artworks/:id/edit', async (req, res) => {// this route is to render an 'EDIT' form
+
+})
+
+app.put('/artworks/:id', async (req, res) => {// later will take user's data from the ‘EDIT’ form
+    const {id} = req.params;
+    const updatedArtwork = await Artwork.findByIdAndUpdate(id, {...req.body.artwork});
+    console.log(updatedArtwork);
+})
+
+app.delete('/artworks/:id', async (req, res) => {
+    const {id} = req.params;
+    await Artwork.findByIdAndDelete(id);
+    res.send('The artwork has been deleted.')
 })
 
 app.listen(3000, () => {
